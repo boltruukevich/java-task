0cde75ee8330d024d94c472763891796d84a722e
Alex Boltrukevich <boltruukevich@gmail.com>
2022-05-31 16:26:03 +0300

    add 2 objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
             PlushBear bear = new PlushBear("I am the bear");
+            PlushBear bear1 = new PlushBear("No, I am the bear");
+            PlushBear bear2 = new PlushBear("No-No, I am first the bear");
             bear.press();
+            bear1.press();
+            bear2.press();
     }
 }

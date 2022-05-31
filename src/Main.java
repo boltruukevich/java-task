3161af9612a88ed77c23b0fcd719a63b0ba86f8f
Alex Boltrukevich <boltruukevich@gmail.com>
2022-05-31 14:32:54 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        PlushBear Bear = new PlushBear("I Am Bear");
+        Bear.Press();
+    }
+}
---
+++ src/PlushBear.java
@@ -0,0 +1,11 @@
+public class PlushBear {
+        String phrase;
+        public PlushBear(String phrase){
+            this.phrase = phrase;
+        }
+        public void Press(){
+            System.out.println(phrase);
+        }
+}
+
+
